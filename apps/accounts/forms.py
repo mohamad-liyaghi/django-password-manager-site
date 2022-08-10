d7a226ff7187b2474b234674ea889685c03ba726
Ml06py <liaghimohamad69@gmail.com>
2022-08-10 14:46:39 +0430

    Allauth register added

--- apps/accounts/forms.py
+++ apps/accounts/forms.py
@@ -1,11 +1,24 @@
 from django import  forms
 from  django.contrib.auth.forms import UserCreationForm
+
+import random
+
 from accounts.models import User
 
 class RegisterForm(forms.ModelForm):
+
     class Meta:
         model = User
-        fields = ('nick_name', 'email', 'token', 'password')
+        fields = ('nick_name', 'email', 'password')
         help_texts = {
             'password' : None,
         }
+
+    def save(self, commit=True):
+        user = super(RegisterForm, self).save(commit=False)
+        user.token = random.randint(1, 99999999999999)
+
+        if commit:
+            user.save()
+
+        return user
--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -5,14 +5,7 @@ from django.urls import reverse_lazy
 from .forms import  RegisterForm
 from django.contrib.auth import logout
 
-class Register_user(CreateView):
-    form_class = RegisterForm
-    template_name = 'accounts/register.html'
-    def form_valid(self, form):
-        form.save()
-        return  redirect('password:home')
-    def form_invalid(self, form):
-        print(form.error)
+
 def logout_user(request):
 	logout(request)
 	return redirect('password:home')
